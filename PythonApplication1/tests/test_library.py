adbb1d75f679229bec4a2f51db2937eaf27ed441
SEVGİ <SEVG@DESKTOP-FLEASTC>
2025-08-14 16:36:55 +0300

    dosya yolu revize edildi

--- PythonApplication1/tests/test_library.py
+++ PythonApplication1/tests/test_library.py
@@ -3,8 +3,8 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pathlib import Path
-from book import Book
-from library import Library
+from src.book import Book
+from src.library import Library
 
 class TestLibrary(unittest.TestCase):
 
